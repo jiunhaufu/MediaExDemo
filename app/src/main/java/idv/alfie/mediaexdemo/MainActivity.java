f5543a2dc076cacd5c74156b1947edffa7cca19a
傅俊皓 <alfie.fu@toplogis.com>
2018-05-15 10:43:13 +0800

    update to Sdk 27

--- app/src/main/java/idv/alfie/mediaexdemo/MainActivity.java
+++ app/src/main/java/idv/alfie/mediaexdemo/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.io.IOException;
